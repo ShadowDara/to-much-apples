b80a6c5f78f6dd210857dc9682a803c969d3f368
ShadowDara <128976697+ShadowDara@users.noreply.github.com>
2025-09-14 17:51:27 +0200

    save

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# CHANGELOG - To much apples mod
+
+## 0.1.0
+
+- added Fabric Version
--- fabric/src/main/java/de/shadowdara/to_much_apples_mod/item/ModItems.java
+++ fabric/src/main/java/de/shadowdara/to_much_apples_mod/item/ModItems.java
@@ -12,9 +12,39 @@ import net.minecraft.util.Identifier;
 public class ModItems {
     public static final Item AMETHYST_APPLE = registerItem("amethyst_apple",
             new Item(new Item.Settings()));
+    public static final Item NETHERITE_APPLE = registerItem("netherite_apple",
+            new Item(new Item.Settings()));
+    public static final Item COPPER_APPLE = registerItem("copper_apple",
+            new Item(new Item.Settings()));
+    public static final Item ORANGE_APPLE = registerItem("orange_apple",
+            new Item(new Item.Settings()));
+    public static final Item LAPIS_APPLE = registerItem("lapis_apple",
+            new Item(new Item.Settings()));
+    public static final Item QUARTZ_APPLEE = registerItem("quartz_apple",
+            new Item(new Item.Settings()));
+    public static final Item GREEN_APPLE = registerItem("green_apple",
+            new Item(new Item.Settings()));
+    public static final Item PINK_APPLE = registerItem("pink_apple",
+            new Item(new Item.Settings()));
+
+    public static final Item NETHERITE_APPLE = registerItem("netherite_apple",
+            new Item(new Item.Settings()));
+
+    public static final Item NETHERITE_APPLE = registerItem("netherite_apple",
+            new Item(new Item.Settings()));
+
+    public static final Item NETHERITE_APPLE = registerItem("netherite_apple",
+            new Item(new Item.Settings()));
 
     private static void addItemsToFoodandDrinkItemGroup(FabricItemGroupEntries entries) {
         entries.add(AMETHYST_APPLE);
+        entries.add(NETHERITE_APPLE);
+        entries.add(COPPER_APPLE);
+        entries.add(ORANGE_APPLE);
+        entries.add(LAPIS_APPLE);
+        entries.add(QUARTZ_APPLEE);
+        entries.add(GREEN_APPLE);
+        entries.add(PINK_APPLE);
     }
 
     private static Item registerItem(String name, Item item) {
